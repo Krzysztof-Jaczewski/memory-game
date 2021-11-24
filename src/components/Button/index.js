c7770e7252110e0726ca85c361525c08a06e07c0
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-11-24 13:55:31 +0100

    Button component created

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,12 @@
+import { useDispatch, useSelector } from "react-redux"
+import { selectCards, shuffleCards } from "../gameSlice"
 import { StyledButton } from "./styled"
 
-
-export  const Button = ()=> (
-    <StyledButton>New Game</StyledButton>
+export  const Button = ()=> {
+const cards = useSelector(selectCards)
+console.log(cards)
+const dispatch = useDispatch()
+return (
+    <StyledButton onClick ={()=>dispatch(shuffleCards())} >New Game</StyledButton>
 )
+}
