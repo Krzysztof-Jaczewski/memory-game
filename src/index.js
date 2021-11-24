fc4cf9612aafd8e4f18f2882042cd07d7ee1129a
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-11-24 13:53:41 +0100

    created store and added Provider to index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom";
 import "./GlobalStyle.js";
 import App from "./App";
 import { GlobalStyle } from "./GlobalStyle";
+import { Provider } from "react-redux";
+import store from "./store.js";
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <GlobalStyle />
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
