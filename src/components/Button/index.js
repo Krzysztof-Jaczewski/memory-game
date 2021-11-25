a04a9a2abf7af39c4e529706b4d7e5acd0bc33a7
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-11-25 15:23:44 +0100

    Button cleaned

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,13 @@
-import { useDispatch, useSelector } from "react-redux"
-import { selectCards, shuffleCards } from "../gameSlice"
-import { StyledButton } from "./styled"
+import {useDispatch} from "react-redux";
+import {shuffleCards} from "../gameSlice";
+import {StyledButton} from "./styled";
 
-export  const Button = ()=> {
-const cards = useSelector(selectCards)
-console.log(cards)
-const dispatch = useDispatch()
-return (
-    <StyledButton onClick ={()=>dispatch(shuffleCards())} >New Game</StyledButton>
-)
-}
+export const Button = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <StyledButton onClick={() => dispatch(shuffleCards())}>
+      New Game
+    </StyledButton>
+  );
+};
