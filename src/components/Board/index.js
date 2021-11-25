6344a2b62612de30d286df0d17df965c6235b675
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-11-25 18:31:59 +0100

    Board content and styling

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,5 +1,20 @@
-import { MainContent } from "./styled";
+import {MainContent, StyledBoard} from "./styled";
+import {useSelector} from "react-redux";
+import {selectCards, selectTurns} from "../gameSlice";
+import {SingleCard} from "../SingleCard";
 
 export const Board = () => {
-  return <MainContent>asd</MainContent>;
+  const cards = useSelector(selectCards);
+  const turns = useSelector(selectTurns);
+
+  console.log(cards, turns);
+  return (
+    <MainContent>
+      <StyledBoard>
+        {cards.map(card => (
+          <SingleCard key={card.id} card={card} />
+        ))}
+      </StyledBoard>
+    </MainContent>
+  );
 };
--- src/components/Board/styled.js
+++ src/components/Board/styled.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 
 export const MainContent = styled.main`
-max-width: 1200px;
-height: 100px;
+  max-width: 540px;
+  min-height: 100px;
+  margin: 40px auto;
+`;
+
+export const StyledBoard = styled.section`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
 `;
