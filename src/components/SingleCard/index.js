20fa5fc31461b7d731843107a600853f2dc2a675
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-11-25 18:35:48 +0100

    single card created and customize

---
+++ src/components/SingleCard/index.js
@@ -0,0 +1,58 @@
+import {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {
+  handleChoice,
+  resetChoice,
+  selectCardBack,
+  selectChoiceOne,
+  selectChoiceTwo,
+  updateCards,
+} from "../gameSlice";
+import {CardFront, CardBack, StyledCard} from "./styled";
+
+export const SingleCard = ({card}) => {
+  const cardBack = useSelector(selectCardBack);
+  const choiceOne = useSelector(selectChoiceOne);
+  const choiceTwo = useSelector(selectChoiceTwo);
+  const dispatch = useDispatch();
+
+  const handleClick = card => {
+    dispatch(handleChoice(card));
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        dispatch(updateCards(choiceOne.src));
+        dispatch(updateCards(choiceTwo.src));
+        dispatch(resetChoice());
+        console.log("brawo");
+      } else {
+        console.log("źle");
+        setTimeout(() => {
+          dispatch(resetChoice());
+        }, 1000);
+      }
+    }
+  }, [dispatch, choiceOne, choiceTwo]);
+
+  return (
+    <StyledCard>
+      <CardFront
+        flipped={
+          card === choiceOne || card === choiceTwo || card.matched
+        }
+        src={card.src}
+        alt="card front"
+      />
+      <CardBack
+        flipped={
+          card === choiceOne || card === choiceTwo || card.matched
+        }
+        src={cardBack.src}
+        alt="card back"
+        onClick={() => handleClick(card)}
+      />
+    </StyledCard>
+  );
+};
---
+++ src/components/SingleCard/styled.js
@@ -0,0 +1,37 @@
+import styled, {css} from "styled-components";
+
+export const StyledCard = styled.div`
+  position: relative;
+`;
+
+export const CardBack = styled.img`
+  width: 100%;
+  display: block;
+  border: 2px solid white;
+  border-radius: 6px;
+  background-image: linear-gradient(green, black);
+  cursor: pointer;
+  transition: all ease-in 0.3s;
+  transition-delay: 0.3s;
+
+  ${({flipped}) =>
+    flipped &&
+    css`
+      transform: rotateY(90deg);
+      transition-delay: 0s;
+    `}
+`;
+
+export const CardFront = styled(CardBack)`
+  transform: rotateY(90deg);
+  position: absolute;
+  transition: all ease-in 0.3s;
+  cursor: not-allowed;
+
+  ${({flipped}) =>
+    flipped &&
+    css`
+      transform: rotateY(0deg);
+      transition-delay: 0.3s;
+    `}
+`;
