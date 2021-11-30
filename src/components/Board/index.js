8e99d2a59ff27eec49867b23cd8b04326ad5a029
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-11-30 10:21:08 +0100

    fixed turns counting , and added conidtion after game end

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,20 +1,50 @@
+import { useEffect } from "react";
 import { MainContent, StyledBoard, Turns } from "./styled";
-import { useSelector } from "react-redux";
-import { selectCards, selectTurns } from "../gameSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  resetChoice,
+  selectCards,
+  selectChoiceOne,
+  selectChoiceTwo,
+  selectIsEveryCardMatched,
+  selectTurns,
+  setDisableTrue,
+  updateCards,
+} from "../gameSlice";
 import { SingleCard } from "../SingleCard";
 
 export const Board = () => {
   const cards = useSelector(selectCards);
   const turns = useSelector(selectTurns);
+  const choiceOne = useSelector(selectChoiceOne);
+  const choiceTwo = useSelector(selectChoiceTwo);
+  const isEveryCardMatched = useSelector(selectIsEveryCardMatched);
+  const dispatch = useDispatch();
 
-  console.log(cards, turns);
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      dispatch(setDisableTrue());
+      if (choiceOne.src === choiceTwo.src) {
+        dispatch(updateCards(choiceOne.src));
+        dispatch(updateCards(choiceTwo.src));
+        dispatch(resetChoice());
+      } else {
+        setTimeout(() => {
+          dispatch(resetChoice());
+        }, 1000);
+      }
+    }
+  }, [dispatch, choiceOne, choiceTwo]);
+
   return (
     <MainContent>
       <Turns>Turn : {turns}</Turns>
       <StyledBoard>
-        {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
-        ))}
+        {isEveryCardMatched && "koniec gry"}
+        {!isEveryCardMatched &&
+          cards.map((card) => (
+            <SingleCard key={card.id} card={card} />
+          ))}
       </StyledBoard>
     </MainContent>
   );
--- src/components/SingleCard/index.js
+++ src/components/SingleCard/index.js
@@ -1,14 +1,10 @@
-import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   handleChoice,
-  resetChoice,
   selectCardBack,
   selectChoiceOne,
   selectChoiceTwo,
-  selectDisabled,
-  setDisabledTrue,
-  updateCards,
+  selectDisable,
 } from "../gameSlice";
 import { CardFront, CardBack, StyledCard } from "./styled";
 
@@ -16,29 +12,14 @@ export const SingleCard = ({ card }) => {
   const cardBack = useSelector(selectCardBack);
   const choiceOne = useSelector(selectChoiceOne);
   const choiceTwo = useSelector(selectChoiceTwo);
-  const disabled = useSelector(selectDisabled);
+  const disable = useSelector(selectDisable);
   const dispatch = useDispatch();
 
   const handleClick = () => {
-    if (!disabled) {
+    if (!disable) {
       dispatch(handleChoice(card));
     }
   };
-
-  useEffect(() => {
-    if (choiceOne && choiceTwo) {
-      dispatch(setDisabledTrue());
-      if (choiceOne.src === choiceTwo.src) {
-        dispatch(updateCards(choiceOne.src));
-        dispatch(updateCards(choiceTwo.src));
-        dispatch(resetChoice());
-      } else {
-        setTimeout(() => {
-          dispatch(resetChoice());
-        }, 1000);
-      }
-    }
-  }, [dispatch, choiceOne, choiceTwo]);
 
   const isFlipped =
     card === choiceOne || card === choiceTwo || card.matched;
@@ -52,10 +33,10 @@ export const SingleCard = ({ card }) => {
       />
       <CardBack
         flipped={isFlipped}
-        disabled={disabled}
+        disable={disable}
         src={cardBack.src}
         alt="card back"
-        onClick={() => handleClick()}
+        onClick={handleClick}
       />
     </StyledCard>
   );
