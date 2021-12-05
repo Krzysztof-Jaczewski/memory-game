ac0c9e39dfd80feb57f2ccc072820237079f3509
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-05 13:45:45 +0100

    Button styling complete

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,13 +1,19 @@
 import { useDispatch } from "react-redux";
 import { startNewGame } from "../gameSlice";
-import { StyledButton } from "./styled";
-
+import { ButtonLabel, Image, StyledButton } from "./styled";
+import SnowCap from "./images/snowCap.svg";
+import SnowPile from "./images/snowPile.svg";
+import Icecle from "./images/icicle.svg";
 export const Button = () => {
   const dispatch = useDispatch();
 
   return (
-    <StyledButton onClick={() => dispatch(startNewGame())}>
-      New Game
-    </StyledButton>
+    <ButtonLabel onClick={() => dispatch(startNewGame())}>
+      <Image src={SnowCap} alt="Snow" />
+      <Image src={SnowPile} alt="Snow" />
+      <Image src={Icecle} alt="Icicle" />
+      <Image src={Icecle} alt="Icicle" />
+      <StyledButton>New Game</StyledButton>
+    </ButtonLabel>
   );
 };
--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,8 +1,49 @@
 import styled from "styled-components";
+// import { ReactComponent as Snow } from "./images/snowCap.svg";
+// import { ReactComponent as Ice } from "./images/icicle.svg";
 
-export const StyledButton = styled.button`
+export const ButtonLabel = styled.label`
+  width: 200px;
+  height: 50px;
   color: black;
   display: block;
   margin: 0 auto 0px;
   padding: 10px;
+  position: relative;
+  &:hover {
+    filter: brightness(120%);
+  }
+`;
+
+export const StyledButton = styled.button`
+  position: absolute;
+  background-color: darkred;
+  width: 100%;
+  height: 100%;
+  border-radius: 20px;
+  color: white;
+  z-index: -1;
+  border: 1px solid darkred;
+`;
+
+export const Image = styled.img`
+  position: absolute;
+  width: 50%;
+  top: 2%;
+  right: 43%;
+
+  &:nth-child(2) {
+    width: 30%;
+    right: -5%;
+  }
+  &:nth-child(3) {
+    width: 15%;
+    top: 90%;
+    right: -6%;
+  }
+  &:nth-child(4) {
+    width: 10%;
+    top: 100%;
+    right: 5%;
+  }
 `;
