954276f0dc292222f7f99fca5fea264087ccdff5
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-05 13:53:34 +0100

    naming changed

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,16 +1,16 @@
 import { useDispatch } from "react-redux";
 import { startNewGame } from "../gameSlice";
 import { ButtonLabel, Image, StyledButton } from "./styled";
-import SnowCap from "./images/snowCap.svg";
-import SnowPile from "./images/snowPile.svg";
+import FirstsnowCap from "./images/snowCap.svg";
+import SecondSnowCap from "./images/snowCap-2.svg";
 import Icecle from "./images/icicle.svg";
 export const Button = () => {
   const dispatch = useDispatch();
 
   return (
     <ButtonLabel onClick={() => dispatch(startNewGame())}>
-      <Image src={SnowCap} alt="Snow" />
-      <Image src={SnowPile} alt="Snow" />
+      <Image src={FirstsnowCap} alt="Snow" />
+      <Image src={SecondSnowCap} alt="Snow" />
       <Image src={Icecle} alt="Icicle" />
       <Image src={Icecle} alt="Icicle" />
       <StyledButton>New Game</StyledButton>
