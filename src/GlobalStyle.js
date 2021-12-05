d52d91a65180c80bd88ebe72a18ba7eff5b3c6f0
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-05 14:30:04 +0100

    Background-image changed

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from "styled-components";
 import lights from "./lights.png";
-import stars from "./stars.jpg";
 export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
@@ -18,10 +17,9 @@ button {
 body {
   height: 90vh;
   font-family: 'Lato', sans-serif;
-  background-image: url(${lights}), url(${stars});
+  background-image: url(${lights}),linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(0,0,8,1) 84%, rgba(8,8,172,1) 100%, rgba(0,212,255,1) 100%);
   background-size: 100% 100%;
   background-attachment: fixed;
   background-repeat: no-repeat;
 }
-
 `;
