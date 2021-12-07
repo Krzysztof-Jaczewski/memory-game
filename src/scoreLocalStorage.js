7f04b225691b4a29f335a76e5d9e4c0fa0e85ddb
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-07 10:59:18 +0100

    created localStorage modifier

---
+++ src/scoreLocalStorage.js
@@ -0,0 +1,5 @@
+export const saveScoreInLocalStorage = (score, text) =>
+  localStorage.setItem(text, JSON.stringify(score));
+
+export const getScoreFromLocalStorage = (text) =>
+  JSON.parse(localStorage.getItem(text)) || [];
