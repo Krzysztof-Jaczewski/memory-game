4a368fafae465178f43a5d7807edd5475109cc81
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-07 18:58:37 +0100

    improved font size of Title

--- src/components/Title/styled.js
+++ src/components/Title/styled.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 export const StyledTitle = styled.h1`
-  color: lightgreen;
-  font-size: 4vw;
+  color: red;
+  font-size: clamp(22px, 4vw, 36px);
   margin: 0;
   white-space: nowrap;
   padding: 0 8px;
 `;
 
 export const Header = styled.header`
-  font-size: 4vw;
+  font-size: clamp(22px, 4vw, 36px);
   display: grid;
   grid-auto-flow: column;
   justify-content: center;
