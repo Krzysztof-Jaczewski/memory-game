24adcca3d1647c64b8bdff30fddc6efa68f75464
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-07 18:57:02 +0100

    added miedia queries to Board

--- src/components/Board/styled.js
+++ src/components/Board/styled.js
@@ -3,18 +3,22 @@ import styled from "styled-components";
 export const MainContent = styled.main`
   max-width: 660px;
   margin: 0 auto 40px;
-  padding: 0 60px;
+  padding: 0 30px;
 `;
 
 export const StyledBoard = styled.section`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  gap: 20px;
+  gap: 18px;
+  position: relative;
+  @media (max-width: 667px) {
+    gap: 8px;
+  }
 `;
 
 export const Turns = styled.p`
   text-align: center;
   padding: 16px;
-  color: lightgreen;
-  font-size: 3vh;
+  color: rgb(255, 0, 0);
+  font-size: clamp(22px, 4vw, 36px); ;
 `;
