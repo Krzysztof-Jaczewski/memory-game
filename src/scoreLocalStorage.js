4363e4707744af49cf50213a3b4f45c964970ece
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-07 22:25:53 +0100

    local storage condition changed

--- src/scoreLocalStorage.js
+++ src/scoreLocalStorage.js
@@ -2,4 +2,4 @@ export const saveScoreInLocalStorage = (score, text) =>
   localStorage.setItem(text, JSON.stringify(score));
 
 export const getScoreFromLocalStorage = (text) =>
-  JSON.parse(localStorage.getItem(text)) || [];
+  JSON.parse(localStorage.getItem(text)) || "no score yet";
