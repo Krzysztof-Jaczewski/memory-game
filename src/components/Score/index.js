208a8f0a32e32876fb693d26334eaa9913d13b66
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-07 18:57:34 +0100

    scoreBoard Created

---
+++ src/components/Score/index.js
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux";
+import { getScoreFromLocalStorage } from "../../scoreLocalStorage";
+import { selectTurns } from "../gameSlice";
+import { Header, ScoreBoard, Text } from "./style";
+
+export const Score = () => {
+  const turns = useSelector(selectTurns);
+  return (
+    <ScoreBoard>
+      <Header>Score</Header>
+      <Text>current score: {turns}</Text>
+      <Header>Your Best Scores</Header>
+      <Text>Beginner: {getScoreFromLocalStorage("beginner")} </Text>
+      <Text>Advenced: {getScoreFromLocalStorage("advanced")}</Text>
+      <Text>Expert: {getScoreFromLocalStorage("expert")}</Text>
+    </ScoreBoard>
+  );
+};
---
+++ src/components/Score/style.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const ScoreBoard = styled.section`
+  position: absolute;
+  width: 80%;
+  left: 10%;
+  top: 20%;
+  padding: 16px 8px;
+  background-color: rgba(0, 0, 100, 0.9);
+  z-index: 1;
+  display: grid;
+  place-items: center;
+  color: red;
+  font-weight: bold;
+  border-radius: 20px;
+  box-shadow: 6px 6px 10px rgb(0, 0, 0),
+    -4px -4px 2px rgb(150, 150, 255),
+    inset -2px -2px 2px rgb(150, 150, 255),
+    inset 2px 2px 5px rgb(0, 0, 0);
+
+  @media (max-width: 500px) {
+    top: 12%;
+    padding: 8px 4px;
+  }
+`;
+
+export const Header = styled.header`
+  font-size: clamp(22px, 4vw, 36px);
+  padding: 16px;
+  @media (max-width: 500px) {
+    padding: 8px;
+  }
+`;
+
+export const Text = styled.p`
+  font-size: clamp(16px, 3vw, 24px);
+  padding: 8px;
+  @media (max-width: 500px) {
+    padding: 4px;
+  }
+`;
