49757041925890ad5d47150304198531836f368b
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-07 22:28:14 +0100

    some formating

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -5,12 +5,13 @@ import { Header, ScoreBoard, Text } from "./style";
 
 export const Score = () => {
   const turns = useSelector(selectTurns);
+
   return (
     <ScoreBoard>
       <Header>Score</Header>
       <Text>current score: {turns}</Text>
       <Header>Your Best Scores</Header>
-      <Text>Beginner: {getScoreFromLocalStorage("beginner")} </Text>
+      <Text>Beginner: {getScoreFromLocalStorage("beginner")}</Text>
       <Text>Advenced: {getScoreFromLocalStorage("advanced")}</Text>
       <Text>Expert: {getScoreFromLocalStorage("expert")}</Text>
     </ScoreBoard>
