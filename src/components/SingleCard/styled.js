68b1dd798f7d34e657042d905bf0b88f13aed0d1
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-12-07 18:58:08 +0100

    added box-shadow to cards

--- src/components/SingleCard/styled.js
+++ src/components/SingleCard/styled.js
@@ -7,12 +7,16 @@ export const StyledCard = styled.div`
 export const CardBack = styled.img`
   width: 100%;
   display: block;
-  border: 2px solid white;
+
   border-radius: 6px;
   background-image: radial-gradient(black, green);
   cursor: pointer;
   transition: all ease-in 0.2s;
   transition-delay: 0.2s;
+  box-shadow: 2px 2px 3px rgb(60, 60, 60),
+    -2px -2px 3px rgb(150, 255, 150),
+    inset -1.5px -1.5px 3px rgb(150, 255, 150),
+    inset 1.5px 1.5px 3px rgb(0, 0, 0);
 
   ${({ flipped }) =>
     flipped &&
